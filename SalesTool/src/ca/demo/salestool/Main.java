fae6f503a7350bba54d3da1f5c7dd47e57c60442
Satyajit Chanda <Satyajit Chanda@10.1.10.19>
2016-08-09 13:14:49 -0500

    salestool commited

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ca.demo.salestool;
+/**
+ * @author Satyajit Chanda
+ *
+ */
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayGeeting();
+	}
+	public static void displayGeeting(){
+		System.out.println("This is first Print1");
+		System.out.println("This is Seound print2");
+		
+		
+	}
+}
